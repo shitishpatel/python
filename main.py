49b0f530444c55fe97b916f28237f8cc489b2f14
shitish <shitishpatel1999@gmail.com>
2020-07-17 19:49:54 -0700

    star pattern

---
+++ main.py
@@ -0,0 +1,180 @@
+# import cv2
+# import math
+
+# print("Hello world\n")
+# print(5+8)
+# print(math.pow(4,2))
+    
+    
+'''
+multi line comment
+
+'''
+
+
+# # single line comment
+
+
+# varible name follow rules like c language
+
+# a=34
+# b="shitish"
+# c="patel"
+# c=23.23
+
+# print(a+c)
+# print(a+2)
+
+# given to type of datatype
+# print(type(a))
+# print(type(b))
+
+
+
+# a="23"
+# a=int(a)
+# print(type(a))
+# print(a+2)
+
+
+# a="23"
+# a=float(a)
+# print(type(a))
+# print(a+2)
+
+
+# b=234
+# b=str(b)
+# print(type(b))
+# print(b)
+
+
+# name="shitish"
+# print(name)
+
+
+# a='patel'
+# print(a)
+
+# b='''vittu
+# is a good boy'''
+# print(b)
+
+
+# name="shitish"
+# print(name[1])
+# print(name[2:5])
+
+
+# a="         shitish         "
+# # print(a) 
+
+# # # for remove extra space
+# # print(a.strip())
+
+# # # for lenght of string
+# # print(len(a))
+# # print(a.upper())
+# # print(a.lower())
+
+
+# print(a.replace("s","a"))
+# print(a.replace(" ","."))
+
+
+
+# a="my name is"
+# b="shitish patel"
+# print(a+" "+b)
+
+
+
+
+# a="shitish"
+# b="aman"
+# # c="my name is {} and my brother name is {}".format(a,b)
+# # c="my name is {1} and my brother name is {0}".format(a,b)
+# # c=f"my name is {a} and my brother name is {b}"
+# c=f"my name is {b} and my brother name is {a}"
+# print(c)
+
+
+
+# operators
+# 1. ** exponential iperator  (power)
+# 2. // floor division operator  (division and gave int value)
+# 3. % modulo operator   (remainder)
+
+
+'''  python collection
+
+1. list             []
+2. tuple            ()
+3. set              {}
+4. dictionary       {}
+
+'''
+
+# a=[2,4,5,6,67,1,56]
+# a[1]=23                  #change the value of list
+# a.append(100)                       insert at last
+# a.insert(2,100)              insert any position
+# a.remove(2)                  remove any position
+# a.pop()                  remove from last
+# del a[2]         delete from position(add.)
+# del a           delete list
+# a.clear()              delete list item
+# a.sort()
+# print(a)
+# print(a[1])
+# print(a[1:4])
+# print(type(a))
+
+
+
+# tuple               we do not change the value of tuple
+# a=(2,3,4,5,9)
+# a=("shitish","aman","arun")
+# a[0]=100           can not do this
+# print(type(a))
+# print(a)
+
+
+
+# a=(1,)         # for one element
+# print(a)
+
+
+
+# set        well define collection
+
+# a={2,3,4,5,6,3,4,5,5,6,6,6,6,3}
+# a.add(23)                     add one element
+# a.update([34,45,4,5,64])          add more than one element
+# print(type(a))
+# print(len(a))               for length
+
+# a.remove(2)            remove element if present in the set else show the error
+# a.discard(34)             remove element if present in the set else do not show error
+
+# .pop, .clear, del , intersection,union       we also use 
+# print(a)
+
+
+
+
+# dictionary is nothing but key value pairs
+
+
+# a={
+#     "name": "shitish",
+#     "college": "bhu",
+#     "rollno.": 45,
+#     "marks": 69.65
+# }                                       use method of dict. with the help of google like del , clear ,pop etc.
+
+# a["name"]="aman"
+# # print(type(a))
+# print(a)
+# print(a["name"])
+
